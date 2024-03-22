e436fca12f3ff40ccf06734a49b73bf0aff5cd47
Mohamed Gallab <mohamed.w.gallab@gmail.com>
2024-03-23 01:29:01 +0200

    added base code

---
+++ traderie/src/main/java/com/massivelyflammableapps/traderie/TraderieApplication.java
@@ -0,0 +1,13 @@
+package com.massivelyflammableapps.traderie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraderieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraderieApplication.class, args);
+	}
+
+}
---
+++ traderie/src/test/java/com/massivelyflammableapps/traderie/TraderieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.massivelyflammableapps.traderie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraderieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
