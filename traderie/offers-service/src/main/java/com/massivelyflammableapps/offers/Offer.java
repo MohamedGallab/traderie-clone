6c193169bfd98a8cef6e04018564dfaa1ada03a1
Mohamed Gallab <mohamed.w.gallab@gmail.com>
2024-04-17 23:09:33 +0200

    cassandra set up

--- traderie/offers-service/src/main/java/com/massivelyflammableapps/offers/Offer.java
+++ traderie/offers-service/src/main/java/com/massivelyflammableapps/offers/Offer.java
@@ -1,11 +1,16 @@
 package com.massivelyflammableapps.offers;
 
 import lombok.*;
+import java.util.UUID;
 
-@Data
-@Builder
+
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+@Table
 public class Offer {
-    private String id;
+    @PrimaryKey
+    private UUID id;
     private String listingId;
     private String buyerId;
     private String sellerId;
