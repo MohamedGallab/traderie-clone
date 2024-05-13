0f9f49322b69d200a38f8fa3755b54469aaebcaa
IrrationalInteger <amrmohamedyonis@gmail.com>
2024-05-13 13:44:05 +0300

    Rebased

---
+++ traderie/listings-service/src/main/java/com/massivelyflammableapps/listings/commands/OffersInvoker.java
@@ -0,0 +1,18 @@
+package com.massivelyflammableapps.listings.commands;
+
+import com.massivelyflammableapps.listings.service.ListingsService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OffersInvoker {
+    @Autowired
+    private ListingsService listingsService;
+
+    @RabbitListener(queues = {"listings-queue"})
+    public <T> T execute(AbstractCommand<T> message) {
+        message.setListingsService(listingsService);
+        return message.execute();
+    }
+}
