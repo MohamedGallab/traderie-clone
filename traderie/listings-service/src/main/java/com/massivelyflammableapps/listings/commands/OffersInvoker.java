9b7d69d93be6325a4c759f99a60205c1de936a85
IrrationalInteger <amrmohamedyonis@gmail.com>
2024-05-13 19:12:39 +0300

    Added Controller

--- traderie/listings-service/src/main/java/com/massivelyflammableapps/listings/commands/OffersInvoker.java
+++ traderie/listings-service/src/main/java/com/massivelyflammableapps/listings/commands/OffersInvoker.java
@@ -1,5 +1,6 @@
 package com.massivelyflammableapps.listings.commands;
 
+import com.massivelyflammableapps.listings.exceptions.UnauthorizedException;
 import com.massivelyflammableapps.listings.service.ListingsService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +12,7 @@ public class OffersInvoker {
     private ListingsService listingsService;
 
     @RabbitListener(queues = {"listings-queue"})
-    public <T> T execute(AbstractCommand<T> message) {
+    public <T> T execute(AbstractCommand<T> message) throws UnauthorizedException {
         message.setListingsService(listingsService);
         return message.execute();
     }
--- traderie/listings-service/src/main/java/com/massivelyflammableapps/listings/controller/ListingsController.java
+++ traderie/listings-service/src/main/java/com/massivelyflammableapps/listings/controller/ListingsController.java
@@ -1,30 +1,20 @@
 package com.massivelyflammableapps.listings.controller;
 
+import com.massivelyflammableapps.listings.commands.*;
+import com.massivelyflammableapps.listings.dto.CreateListingDTO;
+import com.massivelyflammableapps.listings.dto.GetListingsByGameByProductDTO;
+import com.massivelyflammableapps.listings.dto.GetListingsByGameByUserDTO;
+import com.massivelyflammableapps.listings.dto.ListingUpdateDTO;
+import com.massivelyflammableapps.listings.exceptions.UnauthorizedException;
 import com.massivelyflammableapps.listings.service.ListingsService;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.massivelyflammableapps.listings.commands.AbstractCommand;
-import com.massivelyflammableapps.listings.commands.CreateListingCommand;
-import com.massivelyflammableapps.listings.commands.GetAllListingsByGameByProductCommand;
-import com.massivelyflammableapps.listings.commands.GetAllListingsByGameByUserCommand;
-import com.massivelyflammableapps.listings.commands.GetAllMyListingsByGameCommand;
-import com.massivelyflammableapps.listings.commands.OffersInvoker;
-import com.massivelyflammableapps.listings.commands.UpdateListingStateCommand;
+import org.springframework.web.bind.annotation.*;
 import com.massivelyflammableapps.listings.model.ListingByGameByProduct;
 import com.massivelyflammableapps.listings.model.ListingByGameByUser;
-
-import lombok.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
-import java.util.UUID;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("api/v1/listings")
@@ -35,4 +25,75 @@ public class ListingsController {
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
+
+    @GetMapping
+    public ResponseEntity<List<ListingByGameByProduct>> getAllListingsByGameByProduct(@RequestBody GetListingsByGameByProductDTO request){
+        try {
+            AbstractCommand<List<ListingByGameByProduct>> command = new GetAllListingsByGameByProductCommand(listingsService ,request);
+            List<ListingByGameByProduct> listings = rabbitTemplate.convertSendAndReceiveAsType("", "listingsQueue", command,
+                    new ParameterizedTypeReference<List<ListingByGameByProduct>>() {
+                    });
+            return ResponseEntity.ok(listings);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).build();
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ListingByGameByUser>> getAllListingsByGameByUser(@RequestBody GetListingsByGameByUserDTO request){
+        try {
+            AbstractCommand<List<ListingByGameByUser>> command = new GetAllListingsByGameByUserCommand(listingsService, request);
+            List<ListingByGameByUser> listings = rabbitTemplate.convertSendAndReceiveAsType("", "listingsQueue", command,
+                    new ParameterizedTypeReference<List<ListingByGameByUser>>() {
+                    });
+            return ResponseEntity.ok(listings);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).build();
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ListingByGameByUser>> getAllMyListingsByGame(@RequestBody GetListingsByGameByUserDTO request){
+        try {
+            AbstractCommand<List<ListingByGameByUser>> command = new GetAllListingsByGameByUserCommand(listingsService, request);
+            List<ListingByGameByUser> listings = rabbitTemplate.convertSendAndReceiveAsType("", "listingsQueue", command,
+                    new ParameterizedTypeReference<List<ListingByGameByUser>>() {
+                    });
+            return ResponseEntity.ok(listings);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<ListingByGameByProduct> createListing(@RequestBody CreateListingDTO request) {
+        try {
+            AbstractCommand command = new CreateListingCommand(listingsService ,request);
+            ListingByGameByProduct response = rabbitTemplate.convertSendAndReceiveAsType("", "listingsQueue", command,
+                    new ParameterizedTypeReference<ListingByGameByProduct>() {
+                    });
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).build();
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<ListingByGameByProduct> updateListingState(@RequestBody ListingUpdateDTO request) {
+        try {
+            AbstractCommand command = new UpdateListingStateCommand(listingsService ,request);
+            ListingByGameByProduct response = rabbitTemplate.convertSendAndReceiveAsType("", "listingsQueue", command,
+                    new ParameterizedTypeReference<ListingByGameByProduct>() {
+                    });
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).build();
+        }
+    }
+
 }
