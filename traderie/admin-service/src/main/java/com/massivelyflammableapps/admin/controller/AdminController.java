29eb12370da2cfe08f3c0c8054b4974b2287dd0f
Mohamed Gallab <mohamed.w.gallab@gmail.com>
2024-05-16 19:25:31 +0300

    added admin

--- traderie/admin-service/src/main/java/com/massivelyflammableapps/admin/controller/AdminController.java
+++ traderie/admin-service/src/main/java/com/massivelyflammableapps/admin/controller/AdminController.java
@@ -1,4 +1,4 @@
-package com.massivelyflammableapps.Controller;
+package com.massivelyflammableapps.admin.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/controller")
-public class ControllerController {
+public class AdminController {
     @GetMapping
     public void getAllOffers() {
         System.out.println("Hello from controller!");
--- traderie/web-server/src/main/java/com/massivelyflammableapps/webserver/controllers/OffersController.java
+++ traderie/web-server/src/main/java/com/massivelyflammableapps/webserver/controllers/OffersController.java
@@ -49,6 +49,11 @@ public class OffersController {
     @PostMapping
     public ResponseEntity<OfferDTO> createOffer(@RequestBody OfferDTO request) {
         try {
+            // AuthenticateJWTRequest command = new AuthenticateJWTRequest(token);
+            // AuthenticateJWTResponse response = rabbitTemplate.convertSendAndReceiveAsType("", queueName, command,
+            //         new ParameterizedTypeReference<AuthenticateJWTResponse>() {
+            //         });
+            // if not authenticated, return 401
             CreateOfferRequest command = new CreateOfferRequest(request);
             OfferDTO response = rabbitTemplate.convertSendAndReceiveAsType("", queueName, command,
                     new ParameterizedTypeReference<OfferDTO>() {
