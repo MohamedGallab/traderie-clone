20fa14c5853181e9c1a9bac940323dc0bb2f1e82
YousefAllam221b <yousef.allam221b@gmail.com>
2024-05-17 02:15:28 +0300

    created docker compose and configured all services

--- traderie/messages-service/src/main/java/com/massivelyflammableapps/messages/configuration/CassandraConfiguration.java
+++ traderie/messages-service/src/main/java/com/massivelyflammableapps/messages/configuration/CassandraConfiguration.java
@@ -3,6 +3,7 @@ package com.massivelyflammableapps.messages.configuration;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.config.SchemaAction;
@@ -13,9 +14,17 @@ import org.springframework.data.cassandra.repository.config.EnableCassandraRepos
 @Configuration
 @EnableCassandraRepositories(basePackages = "com.massivelyflammableapps")
 public class CassandraConfiguration extends AbstractCassandraConfiguration {
+	@Value("${spring.cassandra.contactpoints}")
+	private String contactPoint;
+
 	@Override
 	public SchemaAction getSchemaAction() {
 		return SchemaAction.CREATE_IF_NOT_EXISTS;
+	}
+
+	@Override
+	protected String getContactPoints() {
+		return contactPoint;
 	}
 
 	@Override
