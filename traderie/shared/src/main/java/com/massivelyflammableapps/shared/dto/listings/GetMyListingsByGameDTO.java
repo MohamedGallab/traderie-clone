2a447156f1be039596fe157cb3e35c849c6dff34
abdelrahmankhaled2001 <abdojarem@gmail.com>
2024-05-18 21:29:39 +0300

    Fixed routing in listings service

---
+++ traderie/shared/src/main/java/com/massivelyflammableapps/shared/dto/listings/GetMyListingsByGameDTO.java
@@ -0,0 +1,14 @@
+package com.massivelyflammableapps.shared.dto.listings;
+
+import lombok.Getter;
+
+import java.util.UUID;
+@Getter
+public class GetMyListingsByGameDTO extends TokenWrapper{
+        private UUID userId;
+        private UUID gameId;
+        private boolean buying;
+        private boolean history;
+    }
+
+
