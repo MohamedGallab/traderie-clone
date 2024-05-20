676051a9c0f13cec8d9b82fbca2b1663491710ab
Mohamed Gallab <mohamed.w.gallab@gmail.com>
2024-05-20 20:58:56 +0300

    added cass

--- traderie/listings-service/src/main/java/com/massivelyflammableapps/listings/configuration/CassandraConfiguration.java
+++ traderie/listings-service/src/main/java/com/massivelyflammableapps/listings/configuration/CassandraConfiguration.java
@@ -1,25 +1,54 @@
 package com.massivelyflammableapps.listings.configuration;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.config.SchemaAction;
+import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
+import org.springframework.data.cassandra.core.cql.keyspace.KeyspaceOption;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 @Configuration
-@EnableCassandraRepositories(basePackages = "com.massivelyflammableapps")
+@EnableCassandraRepositories
 public class CassandraConfiguration extends AbstractCassandraConfiguration {
+	@Value("${spring.cassandra.keyspace-name}")
+	private String keyspaceName;
+
+	@Value("${spring.cassandra.contact-points}")
+	private String contactPoints;
+	
+	@Value("${spring.cassandra.port}")
+	private int port;
+
 	@Override
-    public SchemaAction getSchemaAction() {
-        return SchemaAction.CREATE_IF_NOT_EXISTS;
-    }
+	public SchemaAction getSchemaAction() {
+		return SchemaAction.CREATE_IF_NOT_EXISTS;
+	}
+
+	@Override
+	protected String getContactPoints() {
+		return contactPoints + ":" + port;
+	}
 
 	@Override
 	public String[] getEntityBasePackages() {
-		return new String[]{"com.massivelyflammableapps"};
+		return new String[] { "com.massivelyflammableapps.listings.model" };
 	}
 
 	@Override
 	public String getKeyspaceName() {
-		return "traderie_cassandra";
+		return keyspaceName;
+	}
+
+	@Override
+	protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
+		CreateKeyspaceSpecification specification = CreateKeyspaceSpecification
+				.createKeyspace(keyspaceName).ifNotExists()
+				.with(KeyspaceOption.DURABLE_WRITES, true).withSimpleReplication();
+		return Arrays.asList(specification);
 	}
 }
+
