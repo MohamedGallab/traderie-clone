0269e3f7def07de91e248a69fd33ed4214d02073
Mohamed Gallab <mohamed.w.gallab@gmail.com>
2024-05-26 00:29:58 +0300

    ready

--- traderie/messages-service/src/main/java/com/massivelyflammableapps/messages/MessagesApplication.java
+++ traderie/messages-service/src/main/java/com/massivelyflammableapps/messages/MessagesApplication.java
@@ -3,12 +3,26 @@ package com.massivelyflammableapps.messages;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
 @EnableCaching
+@EnableAsync
 public class MessagesApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MessagesApplication.class, args);
+    }
+
+    @Bean(name = "asyncExecutor")
+    public ThreadPoolTaskExecutor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(1000);
+        executor.setThreadNamePrefix("Async-");
+        executor.initialize();
+        return executor;
     }
 }
--- traderie/users-service/src/main/java/com/massivelyflammableapps/User_Service/UserServiceApplication.java
+++ traderie/users-service/src/main/java/com/massivelyflammableapps/User_Service/UserServiceApplication.java
@@ -3,12 +3,26 @@ package com.massivelyflammableapps.User_Service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CassandraAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication(exclude = CassandraAutoConfiguration.class)
+@EnableAsync
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
+	@Bean(name = "asyncExecutor")
+	public ThreadPoolTaskExecutor asyncExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(10);
+		executor.setMaxPoolSize(10);
+		executor.setQueueCapacity(1000);
+		executor.setThreadNamePrefix("Async-");
+		executor.initialize();
+		return executor;
+	}
 }
